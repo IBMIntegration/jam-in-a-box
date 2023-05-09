fff905ac9f0f667e527d99825870fcb09d7242fe
DebatIBM <112578833+DebatIBM@users.noreply.github.com>
2023-05-09 16:11:36 -0400

    Update Homepage.js

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -12,11 +12,11 @@ const FirstRightText = () => (
       IBM WW Integration SWAT Team offers Jam-in-a-Box which will enable our tech sellers, 
       business partners, clients to reserved a cp4i lab environment on demand ,learn and 
       execute cp4i capabilities from the video enabled presentations along with step-by-step 
-      guide for labs. 
+      guide for labs. <br>
     <div style={{fontStyle: 'italic'}}>
 
   
-      <br>We are committed to help you to take the first step towards Integration Automation learning Journey.
+      We are committed to help you to take the first step towards Integration Automation learning Journey.
     </div>
 
   </p>
