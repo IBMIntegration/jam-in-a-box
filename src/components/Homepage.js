9e28d251dad8cbdaee3f084a4795f23b1d1fd227
DebatIBM <112578833+DebatIBM@users.noreply.github.com>
2023-07-28 12:06:00 -0400

    Update Homepage.js

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -10,11 +10,11 @@ const FirstLeftText = () => <p>Jam-in-a-Box</p>;
 const FirstRightText = () => (
   <p>
       IBM WW Integration SWAT Team offers Jam-in-a-Box which will enable our tech sellers, 
-      business partners to reserve a cp4i lab environment on demand ,learn and 
+      business partners to reserve a CP4I lab environment on demand ,learn and 
       execute cp4i capabilities from the video enabled presentations along with step-by-step 
       guide for labs. 
       <a href="https://ibm.box.com/s/e6n92kczuxphlhp4jl123t44if7bca23">Click here</a>
-    <div style={{fontStyle: 'italic'}}> to listen to the  the audio presenation of cp4i capabilities.{"\n\n\n"}
+     to listen to the  the audio presenation of cp4i capabilities.{"\n\n\n"}
 
   
     
@@ -46,7 +46,7 @@ const customProps = {
     <HomepageCallout
       backgroundColor="#0043ce"
       color="white"
-     /* leftText={FirstLeftText}*/
+      leftText={FirstLeftText}
       rightText={FirstRightText}
     />
   ),
