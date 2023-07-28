02f3e9c7efa9b2f2ed3ccc502e4d513d29357b6b
DebatIBM <112578833+DebatIBM@users.noreply.github.com>
2023-07-28 13:04:38 -0400

    Update Homepage.js

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -10,7 +10,7 @@ const FirstLeftText = () => <p>Jam-in-a-Box</p>;
 const FirstRightText = () => (
   <p>
       IBM WW Integration SWAT Team offers Jam-in-a-Box which will enable our tech sellers, 
-      business partners to reserve a CP4I lab environment on demand ,learn and 
+      business partners to reserve a CP4I lab environment on demand, learn and 
       execute cp4i capabilities from the video enabled presentations along with step-by-step 
       guide for labs. <a href="https://ibm.box.com/s/e6n92kczuxphlhp4jl123t44if7bca23">Click here</a> to
       listen to the  the audio presenation of CP4I capabilities.{"\n\n\n"}
